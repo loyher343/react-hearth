51b78b393cd874f0e0f004b46158d2e1de6c3f08
Loy Hernandez <loyhernandez59@gmail.com>
2017-11-20 16:29:37 -0600

    navbar and home routes setup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+    render(){
+        return(
+            <h1>HearthCards</h1>
+        )
+    }
+}
+
+export default Navbar
